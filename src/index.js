18dafd098c8761cbabf74e483587013492dd89c5
Ivo Ryan <ryanbussines06@gmail.com>
2024-03-24 21:34:09 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -182,7 +182,7 @@ app.post("/user", async (req , res) => {
 });
 
 app.post("/user/:id", async (req , res) => {
-    const user = await new User.findById(req.params.id , {
+    const user = await User.findById(req.params.id , {
         cart: req.body.cart,
     })
     await user.save()
